55191dde660b6cd89a3d7c9e3a6161582741b5fe
fhasnur <fandi.hasnur10@gmail.com>
2024-07-08 17:31:57 +0700

    style: header margin, loading spinner

--- app/ui/home/header.tsx
+++ app/ui/home/header.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Header() {
   return (
-    <header className="flex mt-20 mb-28 items-center justify-center">
+    <header className="flex mt-16 mb-20 items-center justify-center">
       <div className="container mx-auto text-center">
         <h1 className="text-6xl font-semibold leading-normal">
           Discover Inspiration, <br /> Achieve Your Dreams
--- app/ui/home/recent-post.tsx
+++ app/ui/home/recent-post.tsx
@@ -56,7 +56,7 @@ export default function RecentPost() {
 
   if (loading) {
     return (
-      <div className="flex justify-center items-center h-screen">
+      <div className="flex justify-center h-screen">
         <div className="animate-spin rounded-full h-24 w-24 border-t-2 border-b-2 border-gray-900"></div>
       </div>
     );
